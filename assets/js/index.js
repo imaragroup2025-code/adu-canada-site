59904679dcf3fbae4341a96accbad22a8125cfd5
imaragroup2025-code <imaragroup2025@gmail.com>
2026-02-07 22:54:37 -0500

    Delete assets/js/index.js

--- assets/js/index.js
+++
@@ -1,6 +0,0 @@
-const stats = [
-  { value: "100+", label: "Units Installed" },
-  { value: "4.9/5", label: "Customer Rating" },
-  { value: "90 Days", label: "Avg. Completion" },
-  { value: "$200K+", label: "Avg. Value Added" },
-];
